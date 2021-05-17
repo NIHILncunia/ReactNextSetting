892a19a41d605e5c92f572a980b5da0c558405ff
NIHILncunia <nihil_ncunia@naver.com>
2021-05-17 18:23:44 +0900

    2021 0517 head 태그 수정

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Head from 'next/head';
 
 const App = ({ Component, pageProps, }) => {
   return (
     <>
-      <Head>
-        <meta charSet='UTF-8'/>
-      </Head>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,7 +9,11 @@ class NextDocument extends Document {
   render() {
     return (
       <Html lang='ko'>
-        <Head />
+        <Head>
+          <meta charSet='UTF-8'/>
+          <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+          <meta name={'viewport'} content={'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0'} />
+        </Head>
         <body>
           <Main />
           <NextScript />
