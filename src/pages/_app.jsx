a073b0f1b347783d50791e88e5bc12f016761d8a
NIHILncunia <nihil_ncunia@naver.com>
2021-05-19 18:40:25 +0900

    2021 0519 메타태그 조정

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps, }) => {
   return (
     <>
+      <Head>
+        <meta charSet='UTF-8'/>
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <meta name={'viewport'} content={'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0'} />
+      </Head>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,11 +9,7 @@ class NextDocument extends Document {
   render() {
     return (
       <Html lang='ko'>
-        <Head>
-          <meta charSet='UTF-8'/>
-          <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
-          <meta name={'viewport'} content={'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0'} />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
